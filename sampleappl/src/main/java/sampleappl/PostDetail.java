c76eae9f8e635bce28ef6ba8af1d5ceb3af414cc
LENOVO <LENOVO@192.168.0.4>
2021-08-16 17:46:41 +0530

    Post class created

--- sampleappl/src/main/java/sampleappl/PostDetail.java
+++ sampleappl/src/main/java/sampleappl/PostDetail.java
@@ -1,10 +1,9 @@
 package sampleappl;
 
 public class PostDetail {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	String postname;
+	String postdate;
+	
 }
+
+
