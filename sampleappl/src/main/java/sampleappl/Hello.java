53deec03bb97621a30078eacc9ee6fe51470838d
LENOVO <LENOVO@192.168.0.4>
2021-08-16 14:40:40 +0530

    Commit!!

---
+++ sampleappl/src/main/java/sampleappl/Hello.java
@@ -0,0 +1,13 @@
+package sampleappl;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("Afternoon!");
+		System.out.println("Good");
+		System.out.println("WElcom to Git");
+
+	}
+
+}
