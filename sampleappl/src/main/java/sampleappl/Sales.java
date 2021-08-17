b2eaa4c298c0b14aa4df075cfb369ca1639c1cc8
LENOVO <LENOVO@192.168.0.5>
2021-08-17 12:38:26 +0530

    Sales class displayed

--- sampleappl/src/main/java/sampleappl/Sales.java
+++ sampleappl/src/main/java/sampleappl/Sales.java
@@ -1,6 +1,19 @@
 package sampleappl;
 
 public class Sales {
-	private int sales;
-
+	public void display() {
+		int[] arr= {21,45,12,8};
+		int sum=0;
+		for(int num:arr) {
+			sum+=num;
+		
+			System.out.println(num);
+	}
+		System.out.println(sum);
+	
+	}
+	public void print() {
+		System.out.println("Print all array numbers");
+		display();
+	}
 }
